db82a3aca1e7826a279a8baa6f0436ffc92988c0
Luis Zubia <soyzubia@gmail.com>
2024-03-09 05:01:54 -0700

    Include react App in application.js entry to render a random Greeting message retrieved from the api

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import '@hotwired/turbo-rails';
+// import './controllers';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import setupStore from './redux/store';
+import { fetchRandom } from './redux/greetings/greetingsActions';
+
+const appStore = setupStore();
+
+const rootTarget = document.getElementById('root');
+const root = createRoot(rootTarget);
+
+appStore.dispatch(fetchRandom());
+
+root.render(
+  <React.StrictMode>
+    <Provider store={appStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);
